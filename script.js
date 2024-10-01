452be189a8788a7a656b8c5703fd2f9221dc7f32
unknown <rayrestu7@gmail.com>
2024-10-01 21:59:31 +0700

    day one

---
+++ script.js
@@ -0,0 +1,102 @@
+//variabell
+
+// //var
+// //var =
+var nama = "repa"
+var alamat = "jagakarsa"
+var jenisKelamin = "Laki laki"
+var nama = "rey"
+
+// // console.log(nama);
+
+// //let
+// //tidak bisa dideklarasikan ulang
+// let nama = "rey"
+// let alamat = "gandull"
+// console.log(nama);
+// // console.log(alamat);
+// // nama="repa"
+
+
+
+
+// //const
+// //const tidak bisa diubah sama sekali
+// const nama1 = "rey"
+// const alamat1 = "gandul"
+// // console.log(nama);
+// nama1 = "repa"
+
+// const tanggalLahir= 2022;
+
+//==============================================
+
+// Data type (tipedata)
+let nama = "rey" //tipe data string (atau sebagai text)
+// let umur = 17       //tipe data integer(atau bilangan bulat)
+// let umur2 = 17.5    //tipe data decimal
+let isSingle = true //booleaan
+
+// let kalkun = umur + umur2
+// console.log(kalkun);
+
+//undefined
+//null
+
+//tipe data Object
+let dataSiswa = {
+    nama: "rey",   //property
+    alamat: "gandul",
+    jenisKelamin: "Laki laki",
+    umur: 15
+}
+
+
+//tipe data array
+// Index dia merupakan jumlah data  
+// rules of array index dimulai dari 0
+let pilihanGanda = ["a", "b", "c", "d"] //array bisa menyimpan beberapa value
+let pilihanGanda2 = ["a", 12.3, 12, true] //array bisa menyimpan beberapa value
+
+//undefined itu data yg belum atau tdk terdefinisi
+
+console.log(pilihanGanda[4]);
+console.log(pilihanGanda2);
+
+//====================================================
+
+// let name1 = "rey"
+// let alamat1 = "rey"
+// let umur1 = 17
+
+// let name2 = "rey"
+// let alamat2 = "rey"
+// let umur2 = 17
+
+// let name3 = "rey"
+// let alamat3 = "rey"
+// let umur3 = 17
+
+
+//tipe data Array of object
+let dataWarga = [
+    {
+        name1: "rey",
+        alamat1: "gandul",
+        umur1: 17,
+    },
+    {
+        name1: "repa",
+        alamat1: "gandul",
+        umur1: 18,
+    },
+    {
+        name1: "ahmad",
+        alamat1: "pln",
+        umur1: 17,
+    },
+    
+]
+
+// console.log(dataWarga);
+console.log(dataWarga[1].name1);
