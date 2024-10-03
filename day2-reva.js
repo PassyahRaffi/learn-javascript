80afdc5271c27e89b57f1a977b86060affd9d112
ardinsyah-reva <revaardiansyahabdullah@gmail.com>
2024-10-03 23:54:46 +0700

    add day2

---
+++ day2-reva.js
@@ -0,0 +1,107 @@
+//  let nama = ["rey","reva","aziz","bangsamsul","bangpasyah"];
+
+//  nama[3] = "kurniawa
+// nama.push("udin","ahmad","anis","brando","raybau") // menambahkan data array di akhir
+// nama.unshift("dobleh") // menambahkan data baru di bagian pertama
+// nama.splice(1, 3) // menambahkan data di posisi tertentu
+// nama.shift();//menghapus data array bagian awal/index ke 0
+// nama.pop(); // menghapus data array terakhir
+// nama.reverse();
+
+// =====================================================================================
+// manipulasi string
+
+// let text = "Hallo nama saya Agus dan umur saya 12 saya dan saya dan saya"
+// let result = text.replace(/dan/g,"bau")
+
+// let text = "anggun sangat cantik di mata saya";
+// let position = text.search("mata")
+
+// let nama = "bagol"
+// let kecil = nama.toLocaleLowerCase();
+// let besar = nama.toUpperCase();
+
+// console.log(kecil);
+
+// ==============================================================================================
+// ARITMATIKA
+
+// let tambah = 10 + "10";
+// let kurang = 10 - 10;
+// let kali = 10 * "10";
+// let bagi = 10 / 10;
+
+// let hargabarang = 10000;
+// let jumlahbarang = "12";
+
+// let jumblah = (hargabarang + jumlahbarang);
+// console.log(jumblah);
+
+// ===========================================================================================
+// KONDISI
+
+// let umur = 14
+
+// if(umur <= 16){
+//     console.log("belum dewasa");
+// } else{
+//     console.log("dewasa");
+
+// }
+
+// == melakukan perbandingan
+// === melakukan perbandingan sampai ke type data
+
+// let umur = "20"
+
+// if(umur == 20){
+//     console.log("dewasa");
+// }else{
+//     console.log("belum dewasa");
+
+// }
+
+// let hari = "selasa"
+
+// if(hari === "senin"){
+//     console.log("jalan sama circle");
+// }else if(hari === "selasa"){
+//     console.log("jalan sendiri");
+// }else if(hari === "rabu"){
+//     console.log("jalan sama doi");
+// }else{
+//     console.log("jalan sama abang");
+
+// }
+
+// let hari = "jumat";
+
+// switch (hari) {
+//   case "senin":
+//     console.log("jalan sama circle");
+//     break;
+//   case "selasa":
+//     console.log("jalan sama abang");
+//     break;
+//   case " rabu":
+//     console.log("jalan sama doi");
+//     break;
+//   default:
+//     console.log("jalan sendiri");
+// }
+
+// ========================================================================================
+
+// FUNCTION 
+ 
+    // function buatindomie(){
+        
+    //     let mie = "indomie goreng "
+    //     let rasa = "pedas"
+    
+    //     return mie + rasa
+    // }
+     
+    // console.log(buatindomie());
+    
+   
